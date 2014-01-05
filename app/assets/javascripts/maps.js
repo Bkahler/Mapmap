fcac0fa7c39fa9b0af39121dae826550b10561dd
Bryan K <bkahler3@gmail.com>
2014-01-04 17:15:27 -0800

    drawn items fix

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -14,27 +14,49 @@ function initializeMap(){
   // this adds the layer created above to the map //
   map.addLayer(layer);
 
-  var drawnItems = new L.FeatureGroup();
-  map.addLayer(drawnItems);
+var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
 
-  var drawControl = new L.Control.Draw({
-    edit: {
+    var drawControl = new L.Control.Draw({
+      draw: {
+        position: 'topleft',
+        polygon: {
+          title: 'Draw a sexy polygon!',
+          allowIntersection: false,
+          drawError: {
+            color: '#b00b00',
+            timeout: 1000
+          },
+          shapeOptions: {
+            color: '#bada55'
+          },
+          showArea: true
+        },
+        polyline: {
+          metric: false
+        },
+        circle: {
+          shapeOptions: {
+            color: '#662d91'
+          }
+        }
+      },
+      edit: {
         featureGroup: drawnItems
-    }
-  });
-  map.addControl(drawControl);
+      }
+    });
+    map.addControl(drawControl);
 
-  map.on('draw:created', function (e) {
-    var type = e.layerType,
+    map.on('draw:created', function (e) {
+      var type = e.layerType,
         layer = e.layer;
 
-    // if (type === 'marker') {
-    //     // Do marker specific actions
-    // }
+      if (type === 'marker') {
+        layer.bindPopup('A popup!');
+      }
 
-    // Do whatever else you need to. (save to db, add to map etc)
-    map.addLayer(layer);
-});
+      drawnItems.addLayer(layer);
+    });
 
 
 }
